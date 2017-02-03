94a3f5f2c213acf78514d2943f72af268e8e9e9a
Edmundo Alvarez <edmundo@graylog.com>
2017-02-03 17:12:18 +0100

    Remove more external dependencies
    These are either not necessary or required through npm.

--- graylog2-web-interface/src/routing/AppFacade.jsx
+++ graylog2-web-interface/src/routing/AppFacade.jsx
@@ -16,7 +16,7 @@ import 'stylesheets/font-awesome.min.css';
 import 'stylesheets/newfonts.less';
 import 'stylesheets/bootstrap-submenus.less';
 import 'toastr/build/toastr.min.css';
-import 'stylesheets/rickshaw.min.css';
+import 'rickshaw/rickshaw.min.css';
 import 'stylesheets/graylog2.less';
 
 const AppFacade = React.createClass({
